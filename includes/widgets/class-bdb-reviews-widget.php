be7eb45a6a8aec86de92b60dc74e42fd27efad14
Ashley Evans <ashley@nosegraze.com>
2017-04-08 13:27:44 +0100

    Create reviews widget file. #43

---
+++ includes/widgets/class-bdb-reviews-widget.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * class-bdb-reviews-widget.php
+ *
+ * @package   book-database
+ * @copyright Copyright (c) 2017, Ashley Gibson
+ * @license   GPL2+
+ */
+class BDB_Reviews_Widget {
+
+}
